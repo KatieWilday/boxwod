1018b7e1b5c63ac2c2a85cdb69b37aead5a96e0c
Katie Wilday <wildayk@gmail.com>
2020-01-11 18:34:17 -0500

    created home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Katie is the best!
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+
   );
 }
 
