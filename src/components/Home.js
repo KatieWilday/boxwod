effb9412d39ed87af7e9cf632fec96cdd7b7cfd0
Katie Wilday <wildayk@gmail.com>
2020-01-11 18:35:40 -0500

    boxwod

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,14 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+
+export default () =>
+    <div class='Home-wrapper'>
+        <div class='Home-header'>
+            <h1>BoxWOD</h1>
+        </div>
+          <div class='Home-body'>
+
+          </div>
+        <div>
+        </div>
+    </div>
