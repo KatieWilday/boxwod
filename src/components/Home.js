79a2be55994b01350f04e3bd53427c4d1b198f04
Katie Wilday <wildayk@gmail.com>
2020-01-11 19:15:03 -0500

    links

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ import NavBar from './NavBar'
 export default () =>
     <div class='Hcontainer-fluid App-header'>
         <div class='Home-header'>
-            <h1>Today's Date</h1>
+          <h3><Link to = '/previous_wod' className = 'previous-link' activeClassName = 'Active-link'>Previous WOD</Link></h3>
+          <h3><Link to = '/next_wod' className = 'next-link' activeClassName = 'Active-link'>Next WOD</Link></h3>
+          <h1>Today's Date</h1>
             <h2>Current Workout</h2>
         </div>
           <div class='Home-body'>
