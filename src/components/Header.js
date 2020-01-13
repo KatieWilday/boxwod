1fe68521e145410147e5b072b9ad48b2e6647301
Katie Wilday <wildayk@gmail.com>
2020-01-13 17:34:56 -0500

    added greating with time

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,31 @@ import NavBar from './NavBar';
 
 class Header extends React.Component {
   render() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+      fontSize: 30
+    }
+
+    if (hours < 12) {
+      timeOfDay = "Morning"
+      styles.color = "teal"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "Afternoon"
+      styles.color = "yellow"
+    } else {
+      timeOfDay = "Evening"
+      styles.color = "lightBlue"
+    }
+
     return (
         <div className="container-fluid App-header">
           <div className="row">
             <img src={lifter} className="App-logo" alt="logo" />
           </div>
           <div className="row">
-            <h2>Welcome to BoxWOD!</h2>
+            <header className="navbar" style={styles}>Good {` ${timeOfDay},`} Current User!</header>
           </div>
           <NavBar className=""/>
         </div>
